6b0ee2d0726b03328ca16dcc91142c02a5784e05
Jeswin Kumar <jeswinpk@agilehead.com>
2017-10-24 18:09:29 +0530

    Pass root as param

--- src/schemas/write-error.js
+++ src/schemas/write-error.js
@@ -1,48 +1,45 @@
-import { root } from "./";
 import { any, Match, Skip, Fault } from "chimpanzee";
 import { source, composite } from "isotropy-analyzer-utils";
 
-function recurseToParentFromMember(state, analysisState) {
-  return composite(
-    {
+export default function(root) {
+  function recurseToParentFromMember(state, analysisState) {
+    return composite({
       type: "MemberExpression",
       object: source(() => [root, recurseToParent])(state, analysisState)
-    }
-  );
-}
+    });
+  }
 
-function recurseToParentFromCall(state, analysisState) {
-  return composite(
-    {
+  function recurseToParentFromCall(state, analysisState) {
+    return composite({
       type: "CallExpression",
       callee: source(() => [root, recurseToParent])(state, analysisState)
-    }
-  );
-}
+    });
+  }
 
-function recurseToParent(state, analysisState) {
-  return any(
-    [
-      recurseToParentFromMember(state, analysisState),
-      recurseToParentFromCall(state, analysisState)
-    ],
-    { selector: "path" }
-  );
-}
+  function recurseToParent(state, analysisState) {
+    return any(
+      [
+        recurseToParentFromMember(state, analysisState),
+        recurseToParentFromCall(state, analysisState)
+      ],
+      { selector: "path" }
+    );
+  }
 
-export default function(state, analysisState) {
-  return composite(
-    {
-      type: "AssignmentExpression",
-      left: recurseToParent(state, analysisState)
-    },
-    {
-      build: obj => () => result =>
-        result instanceof Match
-          ? new Fault(
-              `Unable to parse KeyValueDB write expression. Refer to documentation.`
-            )
-          : result
-    }
-  );
+  return function(state, analysisState) {
+    return composite(
+      {
+        type: "AssignmentExpression",
+        left: recurseToParent(state, analysisState)
+      },
+      {
+        build: obj => () => result =>
+          result instanceof Match
+            ? new Fault(
+                `Unable to parse KeyValueDB write expression. Refer to documentation.`
+              )
+            : result
+      }
+    );
+  };
 }
