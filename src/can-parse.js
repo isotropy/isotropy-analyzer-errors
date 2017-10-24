8d016888d39a0de339eac7dde8b895cd97370637
Jeswin Kumar <jeswinpk@agilehead.com>
2017-10-24 18:14:56 +0530

    Add dependencies

--- src/can-parse.js
+++ src/can-parse.js
@@ -1,3 +1,5 @@
+import { parse } from "chimpanzee";
+
 export default function canParse(schema, obj) {
   const result = parse(schema)(obj)();
   return result instanceof Match || result instanceof Empty;
